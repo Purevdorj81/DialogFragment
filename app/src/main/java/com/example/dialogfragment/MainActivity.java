b241d919a910abed8ab31019f7480b4e9d5d97e3
MCUD <tsend.purevdorj@gmail.com>
2022-02-22 15:23:46 +0800

    test2-1comment

---
+++ app/src/main/java/com/example/dialogfragment/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.dialogfragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment dialog = FullscreenDialog.newInstance();
+                ((FullscreenDialog) dialog).setCallback(new FullscreenDialog.Callback() {
+                    @Override
+                    public void onActionClick(String name) {
+                        Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.show(getSupportFragmentManager(), "tag");
+            }
+        });
+    }
+}
